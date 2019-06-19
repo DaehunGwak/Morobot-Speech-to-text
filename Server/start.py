e6191f9a3ef37ec9e22545e67c8860fd5be56fcf
곽대훈 <pmon2648@daum.net>
2019-06-19 10:50:21 +0900

    [Edit] Add socket and exec codes.

--- Server/start.py
+++ Server/start.py
@@ -1,5 +1,27 @@
 from record import record_sound
-
+from socket import *
+import os
 
 if __name__ == '__main__':
-    record_sound()
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(('', 5000))
+    sock.listen(1)
+    connSock, addr = sock.accept()
+    print("Accept!")
+
+    try:
+        while True:
+            record_sound()
+            os.system("deepspeech --model models/output_graph.pbmm --alphabet models/alphabet.txt --lm models/lm.binary --trie models/trie --audio file.wav > result.txt")
+            
+            with open("result.txt", "r") as f:
+                lines = f.readlines()
+                print(lines)
+                connSock.send(lines[0].encode('utf-8')) 
+
+    except Exception as ex:
+        print("Error:", ex)
+    
+    connSock.close()
+    sock.close()
+    
